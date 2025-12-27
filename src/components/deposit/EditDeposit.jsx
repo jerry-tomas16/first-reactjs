57a908d8e8f2e71150c7e67e29191ee812387f1d
habibrahmat22 <habibrahmat.mkom27@gmail.com>
2025-12-27 15:16:13 +0700

    update

--- src/components/deposit/EditDeposit.jsx
+++ src/components/deposit/EditDeposit.jsx
@@ -1,141 +1,66 @@
-import { Table, Button, Popconfirm, Tag, Space, Avatar } from "antd";
-import {
-  EyeOutlined,
-  DeleteOutlined,
-  EditOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
+import { Button, Form, Input, Select, Space } from "antd";
 import { useEffect } from "react";
 
-export default function EditDeposit  (props)  {
-  const {
-    deposits,
-    onDelete,
-    setIsDetailOpen,
-    setSelectedDeposit,
-    setOpenEditModal,
-  } = props;
+export default function EditDeposit(props) {
+  const [form] = Form.useForm();
+  const { setIsModalOpen, selectedMenu, handleUpdatedata } = props;
 
-  const text = "Are you sure to delete this Employee data?";
-  const description = "Delete the Deposit data";
+  const validateMessages = {
+    required: "${label} is required!",
+    types: {
+      email: "${label} is not a valid email!",
+      number: "${label} is not a valid number!",
+    },
+    number: {
+      range: "${label} must be between ${min} and ${max}",
+    },
+  };
 
-  const columns = [
-    {
-      title: "No",
-      key: "no",
-      render: (_text, _record, index) => (
-        <div style={{ fontWeight: 500, color: "#666" }}>{index + 1}</div>
-      ),
-      width: 50,
-      align: "center",
-    },
-    {
-      title: "UserId",
-      key: "rekening_id",
-      render: (_text, record) => (
-        <Space>
-          <Avatar
-            style={{ backgroundColor: "#1890ff" }}
-            icon={<UserOutlined />}
-          />
-          <div>
-            <div style={{ fontWeight: 600, color: "#262626" }}>
-              {record.userId}
-            </div>
-            <div style={{ fontSize: "12px", color: "#8c8c8c" }}>
-              {record.rekening_id}
-            </div>
-          </div>
-        </Space>
-      ),
-      sorter: {
-        compare: (a, b) => a.name.localeCompare(b.name),
-        multiple: 3,
-      },
-    },
-    {
-      title: "Nominal",
-      dataIndex: "nominal",
-      width: 150,
-      align: "center",
-      render: (status) => (
-        <Tag color={status === "Active" ? "success" : "default"}>{status}</Tag>
-      ),
-    },
-    {
-      title: "Nominal tips",
-      dataIndex: "nominal_tips",
-      width: 110,
-      align: "center",
-      render: (usia) => <Tag color="blue">{usia}</Tag>,
-    },
-    {
-      title: "Status",
-      dataIndex: "status",
-      width: 110,
-      align: "center",
-      render: (status) => (
-        <Tag color={status === "Aktif" ? "geekblue" : "magenta"}>{status}</Tag>
-      ),
-    },
-    {
-      title: "Action",
-      key: "action",
-      width: 150,
-      align: "center",
-      fixed: "right",
-      render: (_text, record) => (
-        <Space size="small">
-          <Button
-            color="primary"
-            variant="outlined"
-            icon={<EyeOutlined />}
-            onClick={() => {
-              setSelectedDeposit(record);
-              setIsDetailOpen(true);
-            }}
-          />
-          <Button
-            type="default"
-            icon={<EditOutlined />}
-            onClick={() => {
-              setSelectedDeposit(record);
-              setOpenEditModal(true);
-            }}
-          />
-          <Popconfirm
-            placement="leftBottom"
-            title={text}
-            description={description}
-            okText="Yes"
-            cancelText="No"
-            onConfirm={() => {
-              onDelete(record);
-            }}
-          >
-            <Button danger icon={<DeleteOutlined />} />
-          </Popconfirm>
-        </Space>
-      ),
-    },
-  ];
+  useEffect(() => {
+    if (selectedMenu) {
+      form.setFieldsValue({
+        menu: selectedMenu,
+      });
+    }
+  }, [selectedMenu, form]);
+  const onFinish = (values) => {
+    handleUpdatedata(values.menu);
+    form.resetFields();
+    setTimeout(() => {
+      setIsModalOpen(false);
+    }, 100);
+  };
 
   return (
-    <Table
-      columns={columns}
-      dataSource={deposits}
-      bordered
-      size="middle"
-      scroll={{ x: 1000 }}
-      style={{
-        backgroundColor: "#fff",
-        borderRadius: "8px",
-        overflow: "hidden",
-      }}
-      className="custom-table"
-      rowClassName={() => "custom-row"}
-    />
+    <div>
+      <Form
+        form={form}
+        name="form-menu"
+        onFinish={onFinish}
+        layout="vertical"
+        validateMessages={validateMessages}
+        size="middle"
+      >
+        <Form.Item
+          name={["menu", "status"]}
+          label="Status"
+          rules={[{ required: true, message: "Status is required!" }]}
+        >
+          <Select placeholder="Pilih status">
+            <Select.Option value="on_approve">On Approve</Select.Option>
+            <Select.Option value="approved">Approved</Select.Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item style={{ textAlign: "right", marginBottom: 0 }}>
+          <Space>
+            <Button onClick={() => setIsModalOpen(false)}>Cancel</Button>
+            <Button type="primary" htmlType="submit">
+              update
+            </Button>
+          </Space>
+        </Form.Item>
+      </Form>
+    </div>
   );
 }
-
-
