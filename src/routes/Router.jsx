05fee7448dbb04a5e9822539e78ed5219d038dbd
habibrahmat22 <habibrahmat.mkom27@gmail.com>
2025-12-29 14:42:03 +0700

    add protect admin

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate, Outlet} from "react-router-dom";
 import Login from "../pages/authentication/Login";
 import Navbar from "../layouts/Navbar.jsx";
 import Katalog from "../pages/web/Katalog";
@@ -10,6 +10,7 @@ const Home = () => {
 const ProtectedRoute = () => {
   // const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
   // return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
+  return <Outlet />;
 };
 function Router() {
   return (
@@ -17,16 +18,17 @@ function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-
-        <Route path="/menu" element={<Navbar path="/menu" />} />
-        <Route path="/restoran" element={<Navbar path="/restoran" />} />
-        <Route path="/employee" element={<Navbar path="/employee" />} />
-        <Route path="/produk" element={<Navbar path="/produk" />} />
-        <Route path="/stokopname" element={<Navbar path="/stokopname" />} />
-        <Route path="/rekening" element={<Navbar path="/rekening" />} />
-        <Route path="/deposit" element={<Navbar path="/deposit" />} />
-        <Route path="Katalog" element={<Katalog />} />
-        <Route path="topup-saldo" element={<TopupSaldo />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/menu" element={<Navbar path="/menu" />} />
+          <Route path="/restoran" element={<Navbar path="/restoran" />} />
+          <Route path="/employee" element={<Navbar path="/employee" />} />
+          <Route path="/produk" element={<Navbar path="/produk" />} />
+          <Route path="/stokopname" element={<Navbar path="/stokopname" />} />
+          <Route path="/rekening" element={<Navbar path="/rekening" />} />
+          <Route path="/deposit" element={<Navbar path="/deposit" />} />
+          <Route path="Katalog" element={<Katalog />} />
+          <Route path="topup-saldo" element={<TopupSaldo />} />
+        </Route>
         <Route path="*" element={<Navbar path="*" />} />
       </Routes>
       {/* <AuthVerify /> */}
